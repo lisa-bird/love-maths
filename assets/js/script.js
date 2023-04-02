5ebf7b63af08ed597d41048da5312efd4c31ffc2
Birdo12 <lisa.bird@cwa.ac.uk>
2023-04-02 06:53:05 +0000

    Final tidy up for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,12 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+    
     runGame("addition");
 
 });
@@ -21,6 +26,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the users answer has benn processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+     
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
